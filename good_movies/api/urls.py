7876bd2730d3e99fec1cb0e53297d8cf8e733eb3
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-11-27 03:04:06 +0000

    added auth

--- good_movies/api/urls.py
+++ good_movies/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import RegisterView, LoginView, UserView, LogoutView
 
 urlpatterns = [
-    path('', views.getRoutes)
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view()),
+    path('user', UserView.as_view()),
+    path('logout', LogoutView.as_view()),
 ]
--- good_movies/good_movies/settings.py
+++ good_movies/good_movies/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     #needed
     'rest_framework',
+    'corsheaders',
 
     #apps
     'api',
@@ -48,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -130,3 +133,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'api.User'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
