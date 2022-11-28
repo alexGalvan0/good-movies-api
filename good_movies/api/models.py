446a1657dda6ac1b49fc23df79966a02410f0f36
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-11-28 20:13:24 +0000

    update

--- good_movies/api/models.py
+++ good_movies/api/models.py
@@ -15,3 +15,26 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.first_name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    plot = models.TextField(max_length=1000)
+    cast = models.CharField(max_length=255)
+    poster = models.URLField()
+    rated = models.CharField(max_length=10)
+    director = models.CharField(max_length=255)
+    date_released = models.DateField()
+    roten_score = models.IntegerField()
+
+    #added
+    run_time = models.IntegerField()
+    imdbId = models.CharField(max_length=255)
+    year = models.IntegerField()
+
+    #relationships
+    likes = models.ManyToManyField(User, related_name='user_movie_likes')
+    watched = models.ManyToManyField(User, related_name='user_movie_watched')
+    watch_list = models.ManyToManyField(User, related_name='user_movue_watch_list')
+
+    def __str__(self):
+        return f'{self.title}({self.year})'
